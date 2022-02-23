430c856ad88b93a01564e8758f700a3de95ece55
aryelciu001 <aryelciu001@gmail.com>
2022-02-23 21:01:29 +0800

    feat: create hook to handle background image according to theme

--- src/hooks/useBackgroundImage.tsx
+++ src/hooks/useBackgroundImage.tsx
@@ -1,4 +1,11 @@
-export default function useBackgroundImage(bg: string) {
+import { useSelector, useDispatch, RootStateOrAny } from "react-redux";
+
+export default function useBackgroundImage() {
+  const darkTheme = useSelector(
+    (state: RootStateOrAny) => state.global.darkTheme
+  );
+  const bg = darkTheme ? "dark-bg.jpg" : "light-bg.jpg";
+
   return {
     backgroundImage: `url(${process.env.PUBLIC_URL + "/" + bg})`,
     backgroundSize: "cover",
