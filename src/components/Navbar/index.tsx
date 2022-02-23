56f072292aa7e939ff82368d0d9d636ddffc2d4e
aryelciu001 <aryelciu001@gmail.com>
2022-02-23 21:01:07 +0800

    feat: add navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,9 +1,15 @@
-import 'index.scss'
+import { Button } from "@arco-design/web-react";
+import ThemeSwitch from "components/ThemeSwitch";
+import useDualThemeClass from "hooks/useDualThemeClass";
+import "./index.scss";
 
-export default function Navbar () {
+export default function Navbar() {
+  const [CLASS, EL_CLASS] = useDualThemeClass({ main: "navbar", el: "button" });
+
   return (
-    <div className="navbar">
-      
+    <div className={CLASS}>
+      <ThemeSwitch></ThemeSwitch>
+      <Button className={EL_CLASS}>Connect wallet</Button>
     </div>
   );
 }
