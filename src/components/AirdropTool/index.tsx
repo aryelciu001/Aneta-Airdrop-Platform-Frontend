79dca2b71615805d30f32b14d91f35f37f41fc97
aryelciu001 <aryelciu001@gmail.com>
2022-02-25 20:19:12 +0800

    handle upload file containing address

--- src/components/AirdropTool/index.tsx
+++ src/components/AirdropTool/index.tsx
@@ -1,17 +1,24 @@
-import { useState, useEffect } from 'react'
 import TokenSelect from "./TokenSelect";
 import TokenDetail from "./TokenDetail";
 import useDualThemeClass from "hooks/useDualThemeClass";
-import { Button } from "@arco-design/web-react";
+import { Button, Upload } from "@arco-design/web-react";
 import "./index.scss";
+import { useEffect, useRef, useState } from "react";
+import { UploadItem } from "@arco-design/web-react/es/Upload";
 
 export default function AirdropTool() {
   const COMPONENT_CLASS = "airdrop-tool";
+
+  const [fileList, setFileList] = useState<UploadItem[]>([])
 
   const [CLASS, CHILD_CLASS] = useDualThemeClass({
     main: COMPONENT_CLASS,
     el: "child",
   });
+
+  useEffect(() => {
+    // handle file upload
+  }, [fileList])
 
   return (
     <div className={CLASS}>
@@ -19,7 +26,12 @@ export default function AirdropTool() {
         className={`${COMPONENT_CLASS}__token_input ${COMPONENT_CLASS}__row ${CHILD_CLASS}`}
       >
         <TokenSelect />
-        <Button>Add addresses</Button>
+        <Upload showUploadList={false} className={`${COMPONENT_CLASS}__upload`} fileList={fileList} onChange={setFileList}>
+          <Button>Add addresses</Button>  
+        </Upload>
+      </div>
+      <div className={`${COMPONENT_CLASS}__row ${CHILD_CLASS}`}>
+        
       </div>
       <div className={`${COMPONENT_CLASS}__row ${CHILD_CLASS}`}>
         <TokenDetail></TokenDetail>
